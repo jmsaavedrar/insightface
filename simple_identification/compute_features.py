5f7f1486ca610f7f3f6f7a2f42418c886493dc11
jsaa <js.>
2025-05-29 12:11:31 -0400

    insighface

--- simple_identification/compute_features.py
+++ simple_identification/compute_features.py
@@ -1,3 +1,5 @@
 #Read list of images
 #compute features and show
 #visualize
+
+# datos https://www.dropbox.com/scl/fi/d8r23u89vlaym74i25sto/personas.zip?rlkey=bwo587id3j4sb0pgpx8mhuctq&st=808ivdwn&dl=0
